83cf4d04eb93ced3c39619f0644aee5e2fe3022a
TOMProject <yeweigangemail@163.com>
2018-10-21 16:27:48 +0800

    修改ajax 返回的值转换成对象

--- shiro01/src/main/webapp/static/js/myjs.js
+++ shiro01/src/main/webapp/static/js/myjs.js
@@ -1,65 +1,50 @@
 
 $(function(){
-  $('.loginbtn').click(function(){
-	  var username =  document.getElementById("name").value;
-	    alert(username);
-	    var password = document.getElementById("password").value;
-	    alert(password);
-	    var user = {"userName":username,"passWord":password};
-	    var json = JSON.stringify(user);
-	    //var json = $.toJSON(user);
-	    alert(json);
-	    $.ajax({
-			type:"POST",
-			url:"../../user/login",
-			data:json,
-			contentType:"application/json",
-			async:true,
-			success:function(msg){
-				alert(msg);
-				
-			}
-		
-		});
+//  $('.loginbtn').click(function(){
+//	  var username =  document.getElementById("name").value;
+//	    alert(username);
+//	    var password = document.getElementById("password").value;
+//	    alert(password);
+//	    var user = {"userName":username,"passWord":password};
+//	    var json = JSON.stringify(user);
+//	    //var json = $.toJSON(user);
+//	    alert(json);
 //	    $.ajax({
-//	        type:"POST",
-//	        url:"user/login",
-//	        data:json,
-//	        contentType:"application/json",
-//	        async:true,
-//			success:function(data){
-//				alert(data);
-//				alert(data.msg);
+//			type:"POST",
+//			url:"../../user/login",
+//			data:json,
+//			contentType:"application/json",
+//			async:true,
+//			success:function(msg){
+//				alert(msg);
 //			}
-//	       
-//	    });
- 
-  })
+//		});
+//  })
 
 
 });
 
-//function login(){
-//
-//    var loginName =  document.getElementById("name").value;
-//    alert(loginName);
-//    var loginPwd = document.getElementById("password").value;
-//    alert(loginPwd);
-//    var user = {"username":loginName,"password":loginPwd};
-//    var json = JSON.stringify(user);
-//    //var json = $.toJSON(user);
-//    alert(json);
-//    $.ajax({
-//        type:"POST",
-//        url:"user/login",
-//        data:json,
-//        contentType:"application/json",
-//        async:true,
-//		success:function(data){
-//			alert(data);
-//			alert(data.msg);
-//		}
-//       
-//    });
-//
-// }
+function login(){
+
+    var loginName =  document.getElementById("name").value;
+    var loginPwd = document.getElementById("password").value;
+    var user = {"userName":loginName,"passWord":loginPwd};
+    var json = JSON.stringify(user);
+    //var json = $.toJSON(user);
+    $.ajax({
+        type:"POST",
+        url:"../../user/login",
+        data:json,
+        contentType:"application/json",
+        async:true,
+		success:function(data){
+			alert(data);
+			var dataObject = JSON.parse(data);
+			alert(dataObject.code);
+			alert(dataObject.msg);
+			alert(dataObject.data.userName)
+		}
+       
+    });
+
+ }
