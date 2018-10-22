cf956636263052b026c2ea825bb1d87a16e86bb1
Administrator <Administrator@PC-20170809KMOO>
2018-10-22 22:32:21 +0800

    添加用户信息管理，启动tomcat加载数据

--- shiro01/src/main/webapp/static/web/myjs/login.js
+++ shiro01/src/main/webapp/static/web/myjs/login.js
@@ -5,13 +5,10 @@
  */
 function login(){
 	var loginName = $(".loginuser:first").val();
-	alert(loginName);
     var loginPwd = $(".loginpwd:eq(0)").val();
-    alert(loginPwd);
     var user = {"userName":loginName,"passWord":loginPwd};
     var json = JSON.stringify(user);
     //var json = $.toJSON(user);
-    alert(loginPwd);
     $.ajax({
         type:"POST",
         url:"../../user/login",
@@ -19,10 +16,8 @@ function login(){
         contentType:"application/json",
         async:true,
 		success:function(data){
-			alert(data);
+			//alert(data);
 			var dataObject = JSON.parse(data);
-			alert(dataObject.code);
-			alert(dataObject.msg);
 			if(dataObject.code == "0000"){
 				window.location.href="main.html";
 			}
