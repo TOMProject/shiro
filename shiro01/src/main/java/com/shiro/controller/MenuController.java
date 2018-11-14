8b897689951f0eb51f831a4b1248644cce03959f
Administrator <Administrator@PC-20170809KMOO>
2018-11-14 21:51:35 +0800

    添加menucontroll

--- shiro01/src/main/java/com/shiro/controller/MenuController.java
+++ shiro01/src/main/java/com/shiro/controller/MenuController.java
@@ -1,11 +1,29 @@
 package com.shiro.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.shiro.entity.Menu;
+import com.shiro.utils.AjaxResponse;
+import com.shiro.utils.Constant;
 
 @Controller
 @RequestMapping(value="/menu")
 public class MenuController {
-
+	
+	@RequestMapping(value="tree",method=RequestMethod.POST)
+	@ResponseBody
+	public AjaxResponse<List<Menu>> getMenuTree(){
+		AjaxResponse<List<Menu>> ajaxResponse = new AjaxResponse<>(Constant.RS_CODE_ERROR,"获取菜单失败！");
+		
+		
+		
+		return ajaxResponse;
+		
+	}
 	
 }
