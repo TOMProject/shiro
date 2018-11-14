8746c600ca82d0b7fc3a10b13ab325ff667edf9c
Administrator <Administrator@PC-20170809KMOO>
2018-11-14 21:50:55 +0800

    添加menu

--- shiro01/src/main/webapp/static/js/menujs.js
+++ shiro01/src/main/webapp/static/js/menujs.js
@@ -1,7 +1,7 @@
 
 
 $(function(){
-	
+	createMenuTree();
 	
 	
 })
@@ -10,5 +10,31 @@ $("#addMenuModel").click(function(){
 	$('#addUserModal').modal("show")
 })
 	
-	
-	
+function createMenuTree() {
+    $.post(ctx + "menu/tree", {}, function (r) {
+        if (r.code === 0) {
+            var data = r.msg;
+            $('#menuTree').jstree({
+                "core": {
+                    'data': data.children,
+                    'multiple': false
+                },
+                "state": {
+                    "disabled": true
+                },
+                "checkbox": {
+                    "three_state": false
+                },
+                "plugins": ["wholerow", "checkbox"]
+            });
+        } else {
+            $MB.n_danger(r.msg);
+        }
+    })
+
+}	
+
+function getMenu() {
+    var ref = $('#menuTree').jstree(true);
+    $("[name='parentId']").val(ref.get_checked()[0]);
+}	
